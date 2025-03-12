f22e9dff15fc5866705a1cf187607554574940a1
jgtaeeun <jas49258667@gmail.com>
2025-03-12 12:41:46 +0900

    27일차(3/12)조건문,반복문

---
+++ day2/while.c
@@ -0,0 +1,28 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//
+//	int result = 312;
+//
+//	while (1) {
+//		int num;
+//		printf("정수를 입력하세요:");
+//		scanf_s("%d", &num);
+//
+//		if (num == result) {
+//			printf("맞췄습니다!\n");
+//			break;
+//		}
+//		else if (num > result) {
+//			printf("result값은 %d보다 작습니다.\n", num);
+//
+//		}
+//		else {
+//			printf("result값은 %d보다 큽니다.\n", num);
+//
+//		}
+//	
+//	}
+//
+//	return 0;
+//}
